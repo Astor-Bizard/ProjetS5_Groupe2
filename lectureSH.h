395fc9b068c111c766b6ea7f8eed8718fd007e55
bouixl <loic.bouix@iut-valence.fr>
2016-01-05 13:30:21 +0100

    Corrections de bugs

--- lectureSH.h
+++ lectureSH.h
@@ -5,6 +5,6 @@ Lecture de la table des sections
 
 void sectionTypeString(uint32_t sh_type, char* typeString);
 
-char** getSectionsNames(FILE* f, int sectionHeaderCount, uint32_t tableSize, Elf32_Off tableOffset);
+char** getSectionsNames(FILE* f, uint16_t sectionHeaderCount, uint32_t tableSize, Elf32_Off tableOffset);
 
-Elf32_Shdr* lectureSectionHeader(FILE *f, long int sectionHeaderOffset, int sectionHeaderSize, int sectionHeaderCount, int sectionHeaderStringTableIndex, int silent);
+Elf32_Shdr* lectureSectionHeader(FILE *f, Elf32_Off sectionHeaderOffset, uint16_t sectionHeaderSize, uint16_t sectionHeaderCount, uint16_t sectionHeaderStringTableIndex, unsigned char mode_elf, int silent);
--- test.c
+++ test.c
@@ -9,11 +9,11 @@ Lecture de la table des sections
 #include "lectureSH.h"
 
 int main(int argc, char *argv[]) {
-	FILE* f = fopen(argv[1], 'r');
+	FILE* f = fopen(argv[1], "r");
 
 	Elf32_Ehdr elfHeader = lecture_Headers(f);
 
-	Elf32_Shdr* sectionsHeaders = lectureSectionHeader(f, elfHeader.e_shoff, elfHeader.e_shentsize, elfHeader.e_shnum, int elfHeader.e_shstrndx, elfHeader.e_ident[EI_DATA], 0)
+	Elf32_Shdr* sectionsHeaders = lectureSectionHeader(f, elfHeader.e_shoff, elfHeader.e_shentsize, elfHeader.e_shnum, int elfHeader.e_shstrndx, elfHeader.e_ident[EI_DATA], 0);
 
 	fclose(f);
 
