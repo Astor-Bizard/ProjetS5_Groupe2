b1ef98729b0dad34aff29a20983982f2dc2caa13
Gabriel37571 <qsdfghjkwxcvbn@yopmail.com>
2016-01-05 14:16:20 +0100

    Maj lecture_headers.h

--- lecture_headers.h
+++ lecture_headers.h
@@ -15,7 +15,7 @@ Lecture d'un fichier elf et affichage du Header
 	lit nb_octet du fichier f, en little endian si mode = 1, 
 	en big endian si mode = 2
 */
-long long int lireoctets(int mode, FILE *f, int nb_octet);
+long long int lire_octets(int mode, FILE *f, int nb_octet);
 
 /* Prends en parametre un fichier en formet ELF32 ouvert en mode lecture
 et lit toute la partie Headers du fichier
