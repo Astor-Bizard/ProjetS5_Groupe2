974f0063d6bbe4b4a2c87d931c55ec615d24989b
hoangtha <momojean0@gmail.com>
2016-01-05 14:54:55 +0100

    oubli des *

--- lectureST.h
+++ lectureST.h
@@ -9,7 +9,7 @@ Lecture de la table des symboles
 Fonction qui retourne un tableau de symboles
 Si silent est a 1, la fonction n'affichera pas le resultat sur la sortie standard
 */
-Elf32_Sym* lectureSymbolTab(FILE *f, Elf32_Ehdr elfHeader, Elf32_Shdr sectionHeader, int silent);
+Elf32_Sym* lectureSymbolTab(FILE *f, Elf32_Ehdr elfHeader, Elf32_Shdr *sectionHeader, int silent);
 
 /*
 Fonction qui retourne la chaine de caractère qui correspond au type du symbole donné en parametre
@@ -24,5 +24,5 @@ char* bindSymbole(unsigned char bind);
 /*
 Fonction qui initialise les donnée nessessaires au bon fonctionnement de la lecture
 */
-void initSymbolTabUsefullInfo(FILE *f, Elf32_Ehdr elfHeader, Elf32_Shdr sectionHeader, int *size, int *offset);
+void initSymbolTabUsefullInfo(FILE *f, Elf32_Ehdr elfHeader, Elf32_Shdr *sectionHeader, int *size, int *offset);
 #endif
