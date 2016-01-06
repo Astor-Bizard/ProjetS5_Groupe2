02ad7c0cd3bde445dc65f24b9683af030e08e2c2
hoangtha <momojean0@gmail.com>
2016-01-06 11:04:45 +0100

    correction affichage + amélioration

--- lectureST.c
+++ lectureST.c
@@ -30,7 +30,7 @@ Elf32_Sym* lectureSymbolTab(FILE *f, Elf32_Ehdr elfHeader, Elf32_Shdr *sectionHe
 	if (!silent)
 	{
 		printf("Symbol Table: \n");
-		printf("Num:  Value    Size Type    BIND   Other   Name\n");
+		printf("Num:    Value Size Type    Bind   Vis      Ndx Name\n");
 	}
 	int j = 0;
 	int i;
@@ -43,12 +43,12 @@ Elf32_Sym* lectureSymbolTab(FILE *f, Elf32_Ehdr elfHeader, Elf32_Shdr *sectionHe
 		symbolTab[j].st_other = (unsigned char) lire_octets(elfHeader.e_ident[EI_DATA],f,1);
 		symbolTab[j].st_shndx = (uint16_t) lire_octets(elfHeader.e_ident[EI_DATA],f,2);
 
-		unsigned char info = 1<<4 & symbolTab[j].st_info;
-		unsigned char bind = 1&0xf & symbolTab[j].st_info;
+		unsigned char info = 15 & symbolTab[j].st_info;
+		unsigned char bind = 15<<4 & symbolTab[j].st_info;
 
 		if (!silent)
 		{
-			printf("%3d %08x %4d %-7s %-6s %d %x %x\n", j, symbolTab[j].st_value, symbolTab[j].st_size, typeSymbole(info), bindSymbole(bind),
+			printf("%3d  %08x %4d %-7s %-6s %d %d %x\n", j, symbolTab[j].st_value, symbolTab[j].st_size, typeSymbole(info), bindSymbole(bind),
 				symbolTab[j].st_other, symbolTab[j].st_shndx, symbolTab[j].st_name);
 		}
 		j++;
@@ -107,6 +107,38 @@ char* bindSymbole(unsigned char bind)
 	switch(bind)
 	{
 		case STB_LOCAL:
+			bindSymbole = "LOCAL";
+			break;
+		case STB_GLOBAL:
+			bindSymbole = "GLOBAL";
+			break;
+		case STB_WEAK:
+			bindSymbole = "WEAK";
+			break;
+		case STB_LOPROC:
+			bindSymbole = "LOPROC";
+			break;
+		case STB_HIPROC:
+			bindSymbole = "HIPROC";
+			break;
+		default:
+			bindSymbole = "# ERR #";
+			break;
+	}
+	return bindSymbole;
+}
+
+char* visionSymbole(unsigned char vis)
+{
+	char* visSymbole =  (char*) malloc(sizeof(char)*10);
+	if (bindSymbole==NULL) {
+		printf("Erreur lors de l'allocation d'une chaine de type.");
+		return NULL;
+	}
+
+	switch(vis)
+	{
+		case 0:
 			bindSymbole = "LOCAL";
 			break;
 		case STB_GLOBAL:
