52db11164bf62d772ca5da9130f8e9630b87c55a
bouixl <loic.bouix@iut-valence.fr>
2016-01-06 14:42:20 +0100

    Ajout de correctionSymboles.c

---
+++ correctionSymboles.c
@@ -0,0 +1,16 @@
+/*
+Crée le 6 janvier 2016 par Loïc
+Correction des symboles
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <elf.h>
+#include "lecture_headers.h"
+#include "lectureSH.h"
+#include "lectureST.h"
+
+Elf32_Sym* corrigerSymboles(FILE* f, Elf32_Shdr* originalSH, Elf32_Shdr* newSH, Elf32_Sym* oldST) {
+	// TODO
+
+	return NULL;
+}
