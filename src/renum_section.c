00a86629e97b6d4a67892beb0cea505c724b8d7e
Gabriel37571 <qsdfghjkwxcvbn@yopmail.com>
2016-01-07 09:16:02 +0100

    ajout base partie 6

---
+++ src/renum_section.c
@@ -0,0 +1,20 @@
+/*Creer le 6/01/2015 par Jonathan
+Renumerote les section dans un ELF et la table des réimplantations
+*/
+
+/*Op : R_ARM_ABS32, JUMP24 , CALL*/
+
+
+void renumerote_section(FILE *f_read,FILE *f_write,Str_Reloc str_reloc)
+{
+	int i;
+	for(i=0;i<str_reloc.nb_Rel;i++)
+	{
+
+	}
+	for(i=0;i<str_reloc.nb_Rela;i++)
+	{
+
+	}
+
+}
