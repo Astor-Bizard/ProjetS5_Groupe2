19ffdcaccce994cfd8a2eaa85a6a1316618e4eaa
hoangtha <momojean0@gmail.com>
2016-01-07 11:29:04 +0100

    changes

--- src/reimpl_R_ARM_ABS.c
+++ src/reimpl_R_ARM_ABS.c
@@ -10,9 +10,19 @@ Réimplantation de type R_ARM_ABS*
 #include "lecture_headers.h"
 #include "lectureSH.h"
 #include "lectureST.h"
+#include "affichage_relocation.h"
 #include "reimpl_R_ARM_ABS.h"
 
-void reimplantation_R_ARM_ABS()
+void reimplantation_R_ARM_ABS(FILE *f, Str_Reloc tableReloc)
 {
 	// TODO: Faire tout
+	int i;
+	for (i=0; i<tableReloc.nb_Rel; i++)
+	{
+		unsigned char info = 8 & tableReloc.Rel[i].r_info;
+		if( (tableReloc.Rel[i].r_info == 2) || (tableReloc.Rel[i].r_info == 5) || (tableReloc.Rel[i].r_info == 8) )
+		{
+			printf("HOLA une realocation a faire ici qq.");
+		}
+	}
 }
--- src/reimpl_R_ARM_ABS.h
+++ src/reimpl_R_ARM_ABS.h
@@ -4,6 +4,6 @@ Réimplantation de type R_ARM_ABS*
 */
 
 /*
-Fonction principale qui fera la reimplantation de tout les type R_ARM_ABS(16-32)
+Fonction principale qui fera la reimplantation de tout les type R_ARM_ABS(8-16-32)
 */
-void reimplantation_R_ARM_ABS();
+void reimplantation_R_ARM_ABS(FILE *f, Str_Reloc tableReloc);
