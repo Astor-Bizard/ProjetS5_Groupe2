3f074cce4cc9240ceef44b3085720f16c20d4a72
hoangtha <momojean0@gmail.com>
2016-01-07 14:12:56 +0100

    changes
    pourquoi faire (S + A ) | T alors que A est = a 0 et T est a 0
    (parcequ'on ne travaille pas avec des thumb)

--- src/reimpl_R_ARM_ABS.c
+++ src/reimpl_R_ARM_ABS.c
@@ -13,18 +13,27 @@ Réimplantation de type R_ARM_ABS*
 #include "affichage_relocation.h"
 #include "reimpl_R_ARM_ABS.h"
 
-void reimplantation_R_ARM_ABS(FILE *f, Str_Reloc tableReloc)
+void reimplantation_R_ARM_ABS(FILE *f, Str_Reloc tableReloc, ListeSymboles listeDesSymboles)
 {
 	// TODO: Faire tout
 	int i;
+	unsigned char info;
+	uint32_t addrSymbole;
 	for (i=0; i<tableReloc.nb_Rel; i++)
 	{
-		unsigned char info = 8 & tableReloc.Rel[i].r_info;
-		if( (tableReloc.Rel[i].r_info == 2) || (tableReloc.Rel[i].r_info == 5) || (tableReloc.Rel[i].r_info == 8) )
+		info = 255 & tableReloc.Rel[i].r_info;
+		addrSymbole = (16777215<<8 & tableReloc.Rel[i].r_info)>>8;
+
+		if( (info == 2) || (info == 5) || (info == 8) )
 		{
-			printf("HOLA une realocation a faire ici qq.");
+			listeDesSymboles.symbole[i].sh_name
+
+			res = listeDesSymboles.symbole[i].sh_value + tableReloc.Rel[i].addend
 
 			// (S + A) | T , addresse du symbole + relocation, T = 1 si symbole est du type STT_FUNC
+			// S = valeur du symbole 
+			// A = addend de la relocalisation
+			// T = 0
 		}
 	}
 }
