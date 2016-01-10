1cae9a38f4f3d12971027d669508ca4e4dbc5412
Jonathan <jonathan.granier@wanadoo.fr>
2016-01-10 15:34:46 +0100

    Tondu

--- src/modification_ELF.c
+++ src/modification_ELF.c
@@ -19,11 +19,13 @@ Programme principal de la phase 2
 
 int main(int argc, char *argv[])
 {
-	Elf32_Ehdr elfHeaders;
-	Elf32_Shdr *section_headers;
+	Elf32_Ehdr Old_elfHeaders;
+	Elf32_Shdr *Old_section_headers;
+	Elf32_Ehdr New_elfHeaders;
+	Elf32_Shdr New_section_headers;
 	ListeSymboles sym_tab;
 	Str_Reloc str_reloc;
-
+	int i;
 	FILE *f_read, *f_write;
 
 	if(argc != 3)
@@ -47,21 +49,25 @@ int main(int argc, char *argv[])
 	}
 	
 
-	elfHeaders = lecture_Headers(f_read, SILENT);
+	Old_elfHeaders = lecture_Headers(f_read, SILENT);
 	
 	rewind(f_read);
-	section_headers = lectureSectionHeader(f_read, elfHeaders, SILENT);
+	Old_section_headers = lectureSectionHeader(f_read, Old_elfHeaders, SILENT);
 	
 
 	rewind(f_read);
-	sym_tab = lectureSymbolTab(f_read, elfHeaders, section_headers, SILENT);
+	sym_tab = lectureSymbolTab(f_read, Old_elfHeaders, Old_section_headers, SILENT);
 
 	rewind(f_read);
-	str_reloc = affichage_relocation(f_read, elfHeaders, section_headers, sym_tab, SILENT);
-	
+	str_reloc = affichage_relocation(f_read, Old_elfHeaders, Old_section_headers, sym_tab, SILENT);
+	for(i=0;i<Old_elfHeaders.e_shnum;i++)
+	{
+		printf("Nom : %06x\n",Old_section_headers[i].sh_offset);
+	}
 	printf("-----Fin de la lecture de %s-----\n",argv[1]);
 	rewind(f_read);
-	renumerote_section(f_read,f_write,&elfHeaders, section_headers,sym_tab,str_reloc);
+	renumerote_section(f_read,f_write,Old_elfHeaders, Old_section_headers,
+		sym_tab,str_reloc, &New_elfHeaders,&New_section_headers);
 
 	printf("-----Fin de l'ecriture dans %s-----\n",argv[2]);
 	return 0;
