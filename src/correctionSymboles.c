1414c36da5a3427e3c684d8e4e7168191d4425ef
bouixl <loic.bouix@iut-valence.fr>
2016-01-12 15:37:22 +0100

    Boulgour à la fraise et au camembert

--- src/correctionSymboles.c
+++ src/correctionSymboles.c
@@ -67,30 +67,29 @@ ListeSymboles applySymbolsCorrections(FILE* oldFile, Elf32_Ehdr oldElfHeader, El
 			newSymbolsTable.symboles[j-d].st_shndx = index_Shdr(originalName, newElfHeader, newSHList);
 			free(originalName);
 		}
-		if(newSymbolsTable.symboles[j-d].st_shndx==-1) {
+		if(newSymbolsTable.symboles[j-d].st_shndx==65535) {
 			d++;
+			continue;
 		}
-		else {
-			// Nouvelle valeur du symbole = Ancienne valeur + Adresse de la section parente
-			newSymbolsTable.symboles[j-d].st_value = oldSymbolsTable.symboles[j].st_value + newSHList.headers[i].sh_addr;
+		// Nouvelle valeur du symbole = Ancienne valeur + Adresse de la section parente
+		newSymbolsTable.symboles[j-d].st_value = oldSymbolsTable.symboles[j].st_value + newSHList.headers[i].sh_addr;
 
-			// Affichage si necessaire
-			if (!silent)
+		// Affichage si necessaire
+		if (!silent)
+		{
+			info = 15 & newSymbolsTable.symboles[j-d].st_info;
+			bind = 15<<4 & newSymbolsTable.symboles[j-d].st_info;
+			symbolName = getSymbolNameBis(newSymbolsTable.names, newSymbolsTable.symboles[j-d]);
+
+			if(newSymbolsTable.symboles[j-d].st_shndx == 0)
 			{
-				info = 15 & newSymbolsTable.symboles[j-d].st_info;
-				bind = 15<<4 & newSymbolsTable.symboles[j-d].st_info;
-				symbolName = getSymbolNameBis(newSymbolsTable.names, newSymbolsTable.symboles[j-d]);
-
-				if(newSymbolsTable.symboles[j-d].st_shndx == 0)
-				{
-					printf("   %3d: %08x %5d %-7s %-6s %-7s  UND %s\n", j, newSymbolsTable.symboles[j-d].st_value, newSymbolsTable.symboles[j-d].st_size, typeSymbole(info), bindSymbole(bind), visionSymbole(newSymbolsTable.symboles[j-d].st_other), symbolName);
-				}
-				else
-				{
-					printf("   %3d: %08x %5d %-7s %-6s %-7s  %3d %s\n", j, newSymbolsTable.symboles[j-d].st_value, newSymbolsTable.symboles[j-d].st_size, typeSymbole(info), bindSymbole(bind), visionSymbole(newSymbolsTable.symboles[j-d].st_other), newSymbolsTable.symboles[j-d].st_shndx, symbolName);
-				}
-				free(symbolName);
+				printf("   %3d: %08x %5d %-7s %-6s %-7s  UND %s\n", j, newSymbolsTable.symboles[j-d].st_value, newSymbolsTable.symboles[j-d].st_size, typeSymbole(info), bindSymbole(bind), visionSymbole(newSymbolsTable.symboles[j-d].st_other), symbolName);
 			}
+			else
+			{
+				printf("   %3d: %08x %5d %-7s %-6s %-7s  %3d %s\n", j, newSymbolsTable.symboles[j-d].st_value, newSymbolsTable.symboles[j-d].st_size, typeSymbole(info), bindSymbole(bind), visionSymbole(newSymbolsTable.symboles[j-d].st_other), newSymbolsTable.symboles[j-d].st_shndx, symbolName);
+			}
+			free(symbolName);
 		}
 	}
 
