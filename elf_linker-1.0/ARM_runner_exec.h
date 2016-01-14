2cdda207e45fc685672618630a2917a8d3843f88
Jylomaki <gaby.denoix@gmail.com>
2016-01-14 16:15:23 +0100

    Comments are real thing.

--- elf_linker-1.0/ARM_runner_exec.h
+++ elf_linker-1.0/ARM_runner_exec.h
@@ -6,6 +6,9 @@
 #include "../src/lecture_headers.h"
 
 
-
+// Se connecte au simulatueur et lance la simulation du fihcier.
 void run(char *hostname, char *servicename, FILE* f);
+
+//Lit le fichier, renvoie l'adresse de départ,
+//et taille du fichier a implanter dans le simulatueur
 void lecture_fichier(FILE *f, uint32_t *address, size_t *size);
